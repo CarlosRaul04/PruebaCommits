077209b8aaf0ace834da6b20be2532923351b1fd
CarlosRaul04 <carlos.ramirez.cuentas04@gmail.com>
2025-09-17 09:49:32 -0500

    feat(auth): add refresh token flow
    add refresh token flow

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true, // usar comillas simples
+  semi: true, // forzar punto y coma
+  trailingComma: 'all', // comas al final de arrays/objetos
+  printWidth: 100, // ancho máximo de línea
+};
